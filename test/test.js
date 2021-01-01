0e065613c34d22d89155aa5ab6b5a9c51a1b5543
chocolateboy <chocolate@cpan.org>
2021-01-01 16:26:27 +0000

    Fix the regex-based name scraper
    Remove the trailing space after the name extracted from
    `Function#toString`.
    
    Setup:
    
        var anon = function (fn) { return fn }
    
        var Person = anon(function (name, age) {
            this.name = name
            this.age = age
        })
    
        Person.toString = function () {
            return 'function Person () { }'
        }
    
        var person = new Person('Alice', 42)
    
    Before:
    
        typeName(person) // "Person "
    
    After:
    
        typeName(person) // "Person"

--- test/test.js
+++ test/test.js
@@ -1,43 +1,60 @@
+var assert = require('assert');
+var typeName = require('..');
+var woothee = require('woothee');
+
+// use indirection to avoid name assignment in ES6
+function anon (fn) {
+    assert(!fn.name);
+    return fn;
+}
+
 function Person(name, age) {
     this.name = name;
     this.age = age;
 }
 
-var AnonPerson = function(name, age) {
+var AnonymousPerson = anon(function(name, age) {
     this.name = name;
     this.age = age;
+});
+
+var PseudonymousPerson = anon(function(name, age) {
+    this.name = name;
+    this.age = age;
+});
+
+PseudonymousPerson.toString = function () {
+    return 'function PseudonymousPerson () { }';
 };
 
-var typeName = require('..'),
-    woothee = require('woothee'),
-    assert = require('assert'),
-    fixtures = {
-        'string literal': 'foo',
-        'number literal': 5,
-        'boolean literal': false,
-        'regexp literal': /^not/,
-        'array literal': ['foo', 4],
-        'object literal': {name: 'bar'},
-        'function expression': function () {},
-        'String object': new String('foo'),
-        'Number object': new Number('3'),
-        'Boolean object':new Boolean('1'),
-        'Date object': new Date(),
-        'RegExp object': new RegExp('^not', 'g'),
-        'Array object': new Array(),
-        'Object object': new Object(),
-        'Function object': new Function('x', 'y', 'return x + y'),
-        'Error object': new Error('error!'),
-        'TypeError object': new TypeError('type error!'),
-        'RangeError object': new RangeError('range error!'),
-        'user-defined constructor': new Person('alice', 5),
-        'anonymous constructor': new AnonPerson('bob', 4),
-        'NaN': NaN,
-        'Infinity': Infinity,
-        'Math': Math,
-        'null literal': null,
-        'undefined value': undefined
-    };
+var fixtures = {
+    'string literal': 'foo',
+    'number literal': 5,
+    'boolean literal': false,
+    'regexp literal': /^not/,
+    'array literal': ['foo', 4],
+    'object literal': {name: 'bar'},
+    'function expression': function () {},
+    'String object': new String('foo'),
+    'Number object': new Number('3'),
+    'Boolean object':new Boolean('1'),
+    'Date object': new Date(),
+    'RegExp object': new RegExp('^not', 'g'),
+    'Array object': new Array(),
+    'Object object': new Object(),
+    'Function object': new Function('x', 'y', 'return x + y'),
+    'Error object': new Error('error!'),
+    'TypeError object': new TypeError('type error!'),
+    'RangeError object': new RangeError('range error!'),
+    'user-defined constructor': new Person('alice', 5),
+    'anonymous constructor': new AnonymousPerson('bob', 4),
+    'pseudonymous constructor': new PseudonymousPerson('nemo', 3),
+    'NaN': NaN,
+    'Infinity': Infinity,
+    'Math': Math,
+    'null literal': null,
+    'undefined value': undefined
+};
 
 function addJsonSuite (tests, fixtures) {
     if (typeof JSON === 'undefined') {
@@ -90,6 +107,7 @@ describe('typeName of', function () {
         ['Math',                     'Math'],
         ['user-defined constructor', 'Person'],
         ['anonymous constructor',    ''],
+        ['pseudonymous constructor', 'PseudonymousPerson'],
         ['null literal',             'null'],
         ['undefined value',          'undefined']
     ];
